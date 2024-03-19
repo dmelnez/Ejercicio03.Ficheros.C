fa7fd32102e1b068c0d09c5c76c225a13b63b068
DESKTOP-JV3V0TT\dmelnez <csi23-dmelnez@colegioaltair.net>
2024-03-19 13:40:57 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio03.Ficheros.C/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Ejercicio03.Ficheros.C
+{
+
+    class Program
+    {
+
+      public static void Main(string[] args)
+        {
+            string rutaArchivo = "C:\\Users\\dmelnez\\Desktop\\ActividadEjercicioC.txt";
+
+            Console.WriteLine("NUMERO DE LINEA: ");
+            int numeroLineaUsu = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("TEXTO A REEMPLAZAR: ");
+            string textoAReemplazar = Console.ReadLine();
+
+
+            try { 
+
+            string[] numeroLineas = File.ReadAllLines(rutaArchivo);
+
+
+             numeroLineas[numeroLineaUsu -1] = textoAReemplazar;
+
+
+
+                File.WriteAllLines(rutaArchivo, numeroLineas);
+
+            }
+
+            catch
+            {
+
+                Console.WriteLine("[INFO] -> Se ha Producido Un Fallo");
+
+            }
+        }
+
+
+    }
+
+}
